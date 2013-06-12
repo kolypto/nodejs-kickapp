a3a6f286b7aff34fe0e86e52fc2b92dc52d0e0fe
Vartanyan Mark <kolypto@gmail.com>
2013-06-12 17:49:25 +0300

    Initial release

---
+++ README.md
@@ -0,0 +1,103 @@
+KickApp
+=======
+
+KickApp is an application architecture framework with pluggable services.
+
+Organize your application into interdependent hierarchical services embedded 
+into the top-level `Application` object, `app.start()` it and enjoy the *ordnung*!
+
+Documentation
+=============
+
+
+The `Application` component
+---------------------------
+
+An `Application` is the root for all services, capable of containing other services. Internally, an `Application` is
+a `ServiceContainer`.
+
+You define an application by creating an instance of it, providing a constructor function as an argument.
+
+By design, an `Application` does not have custom start/stop behavior: instead, it wraps services.
+
+    :::js
+    var nconf = require('nconf');
+
+    nconf.argv().file('config.json');
+
+    var app = new kickapp.Application(function(){
+        this.nconf = nconf;
+    });
+
+Note that the the constructor has no options: being the root object, it uses variables of the outer scope.
+
+Assuming you have some services defined, you attach them to the applcation and start all of them by starting the application:
+
+    :::js
+    var services = require('./services');
+
+    app.addChild('db', services.DbService, { host: 'localhost' });
+
+    app.start(function(err){
+        if (err)
+            console.error('Failed to start: ', err);
+        else
+            console.log('Started successfully');
+    });
+
+`Service` Interface
+-------------------
+
+A service is anything that implements the `Service` interface:
+
+* Its constructor receives two arguments:
+
+    * `app` is the root `Application` object
+    * `options` is a custom configuration object
+
+* Must have a `start(callback)` method which launches the service asynchronously
+* Must have a `stop(callback)` method which halts the service asynchronously
+* The `callback` is a `function(err:String?, result)` used to report success or failure.
+* Being part of the `kickapp` framework, it receives the `service` property with its `ServiceContainer` (see below)
+
+A service interface is defined in `kickapp.Service`. You do not necessarily inherit from it:
+implementing two methods is just enough.
+
+Internally, when you `start()` an `Application`, all child services are `start()`ed with the help of
+[`async.series`](https://github.com/caolan/async).
+
+Here's a naive example of a service:
+
+    :::js
+    var DbService = function(app, options){
+        this.db = new DB(options.host); //
+    };
+
+    DbService.prototype.start = function(callback){
+        var promise = this.db.connect(callback);
+    };
+
+    DbService.prototype.stop = function(callback){
+        this.db.disconnect();
+        callback();
+    };
+
+Note that you never instantiate a service manually: kickapp framework does this for you.
+
+The `ServiceContainer` component
+--------------------------------
+
+A `ServiceContainer` is an object that wraps a `Service` along with its related data, including:
+
+* `name` of the service
+* A list of child services: `children`
+* The `parent` service
+* Whether the service is `running`
+
+It also defines the following methods:
+
+* `start(callback)`, which launches the service itself and its child services
+* `stop(callback)`, which halts the service and its child services
+* `addChild(name, Service, options)` wrapper that creates a child `Service(app, options)`, wraps it with a
+`ServiceContainer` and maintains the parent-child relationship.
+* `getService(name)` allows you to get a `Service` by name. Dot-notation is supported to get child services
---
+++ tests/startstop-test.js
@@ -0,0 +1,102 @@
+'use strict';
+
+var vows = require('vows'),
+    assert = require('assert'),
+    util = require('util'),
+    kickapp = require('..');
+
+var Environment = function(){
+    // Custom application
+    this.app = new kickapp.Application(function(){
+        this.config = {a:1, b:2};
+    });
+
+    var events = this.events = [];
+
+    // Custom service
+    var Service = function(app, options){
+        this.name = options.name;
+    };
+    Service.prototype.start = function(finish){
+        events.push('+' + this.name);
+        finish();
+    };
+    Service.prototype.stop = function(finish){
+        events.push('-' + this.name);
+        finish();
+    };
+
+    // Hierarchy
+    var s1 = this.app.addChild('s1', Service, {name: 's1'});
+    var s2 = this.app.addChild('s2', Service, {name: 's2'});
+    var s11 = s1.addChild('s11', Service, {name: 's11'});
+    var s111 = s11.addChild('s111', Service, {name: 's111'});
+    var s112 = s11.addChild('s112', Service, {name: 's112'});
+};
+
+vows.describe('Application') // test suite
+    .addBatch({
+        // contexts
+        'when initialized': {
+            // input
+            topic: function(){
+                return new Environment();
+            },
+            // run tests against the input
+            'no errors should occur': function(topic){
+            },
+            'no events were emitted': function(topic){
+                assert.equal(topic.events.length, 0);
+            },
+            'structure is correct': function(topic){
+                assert.equal(Object.keys(topic.app.children).length, 2);
+                assert.deepEqual(
+                    ['s1','s2','s1.s11','s1.s11.s111', 's1.s11.s112'].map(function(name){
+                        return Object.keys(topic.app.getService(name).service.children).length;
+                    }),
+                    [1,0,2,0,0]
+                );
+            },
+
+            // child scope
+            'and started': {
+                topic: function(env){
+                    env.app.start();
+                    return env;
+                },
+
+                'emitted events should match the set': function(topic){
+                    assert.deepEqual(topic.events, ['+s1', '+s11', '+s111', '+s112', '+s2']);
+                },
+                'all services are running': function(topic){
+                    assert.deepEqual(
+                        ['','s1','s2','s1.s11','s1.s11.s111', 's1.s11.s112'].map(function(name){
+                            return name? topic.app.getService(name).service.running : topic.app.running;
+                        }),
+                        [true, true, true, true, true, true]
+                    );
+                },
+
+                'then stopped': {
+                    topic: function(env){
+                        env.app.stop();
+                        return env;
+                    },
+
+                    'emitted events should match the set': function(topic){
+                        assert.deepEqual(topic.events, ['+s1', '+s11', '+s111', '+s112', '+s2', '-s1', '-s11', '-s111', '-s112', '-s2']);
+                    },
+                    'all services are not running': function(topic){
+                        assert.deepEqual(
+                            ['', 's1','s2','s1.s11','s1.s11.s111', 's1.s11.s112'].map(function(name){
+                                return name? topic.app.getService(name).service.running : topic.app.running;
+                            }),
+                            [false, false, false, false, false, false]
+                        );
+                    }
+                }
+            }
+        }
+    })
+    //.run();
+    .export(module);
