66bf3ba111b7eb5c42a0ce616fb80f3dca0058aa
Vartanyan Mark <kolypto@gmail.com>
2013-12-13 15:50:56 +0200

    Application, ServiceWrapper: structure & tests

--- tests/Application-test.js
+++ tests/Application-test.js
@@ -27,15 +27,36 @@ exports.testApplication = function(test){
     }).promisedMode(true);
 
     // Test structure
+    test.deepEqual(_.keys(app._services), ['d1', 'd2', 'd3', 'd4', 'z']);
+    test.deepEqual(app.getServiceWrapper('d1').dependencies, []);
+    test.deepEqual(app.getServiceWrapper('d2').dependencies, ['d1']);
+    test.deepEqual(app.getServiceWrapper('d3').dependencies, ['d1', 'd2']);
+    test.deepEqual(app.getServiceWrapper('d4').dependencies, ['d3']);
+    test.deepEqual(app.getServiceWrapper('z').dependencies, []);
+
+    // Test services
+    test.deepEqual(app.get('d1'), { args: ['a','b'], methods: [] });
+    test.deepEqual(app.get('d2'), { args: ['c','d'], methods: [] });
+    test.deepEqual(app.get('d3'), { args: ['e','f'], methods: [] });
+    test.deepEqual(app.get('d4'), { args: ['g','h'], methods: [] });
+    test.deepEqual(app.get('z'),  { args: ['z'],     methods: [] });
+
+    // Test toposorted sequence
+    test.deepEqual(
+        _.pluck(app._servicesSequence(), 'name'),
+        ['d1', 'd2', 'd3', 'd4', 'z']
+    );
 
     // Prepare the event listeners
     var events = new u.EventsCollector();
     events.listen('app', app, ['init', 'start', 'stop']);
-    events.listen('d1', app.getServiveWrapper('d1'), ['init', 'start', 'stop']);
-    events.listen('d2', app.getServiveWrapper('d2'), ['init', 'start', 'stop']);
-    events.listen('d3', app.getServiveWrapper('d3'), ['init', 'start', 'stop']);
-    events.listen('d4', app.getServiveWrapper('d4'), ['init', 'start', 'stop']);
-    events.listen('z',  app.getServiveWrapper('z'),  ['init', 'start', 'stop']);
+    events.listen('d1', app.getServiceWrapper('d1'), ['init', 'start', 'stop']);
+    events.listen('d2', app.getServiceWrapper('d2'), ['init', 'start', 'stop']);
+    events.listen('d3', app.getServiceWrapper('d3'), ['init', 'start', 'stop']);
+    events.listen('d4', app.getServiceWrapper('d4'), ['init', 'start', 'stop']);
+    events.listen('z',  app.getServiceWrapper('z'),  ['init', 'start', 'stop']);
+
+    return test.done();
 
     // Run tests
     return [
